7d7bcab0fd05c4ac1c6bd84a118c4183429f862d
yangcong <690051852@qq.com>
2023-03-04 16:01:55 +0800

    增加系统异常和请求方式异常处理

--- sky-framework-config/src/main/java/org/sky/framework/config/exception/GlobalExceptionHandler.java
+++ sky-framework-config/src/main/java/org/sky/framework/config/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.sky.framework.api.common.result.AjaxResult;
 import org.sky.framework.config.exception.business.BusinessException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -27,8 +28,30 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(BusinessException.class)
     public Object business(BusinessException businessException, HttpServletRequest request){
-        log.error("业务异常 ===>>> %s , 业务类型 %s", businessException.getMessage(), businessException.getClass().getName());
+        log.error("业务异常 ===>>> '{}' , 业务类型 '{}' ", businessException.getMessage(), businessException.getClass().getName());
         return AjaxResult.error(500, null, businessException.getMessage());
     }
 
+    /**
+     * 请求方式不支持
+     */
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public AjaxResult handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException e,
+                                                          HttpServletRequest request)
+    {
+        String requestURI = request.getRequestURI();
+        log.error("请求地址'{}',不支持'{}'请求", requestURI, e.getMethod());
+        return AjaxResult.error(405, null, e.getMessage());
+    }
+
+    /**
+     * 系统异常
+     */
+    @ExceptionHandler(Exception.class)
+    public AjaxResult handleException(Exception e, HttpServletRequest request)
+    {
+        String requestURI = request.getRequestURI();
+        log.error("请求地址'{}',发生系统异常.", requestURI, e);
+        return AjaxResult.error(500, null, e.getMessage());
+    }
 }
