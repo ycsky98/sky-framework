5fd954d4116a6e65ab67c86a6505d1c28e6e7f1a
yangcong <690051852@qq.com>
2023-04-11 15:02:46 +0800

    修改处理redis序列化LocalDateTime问题

--- sky-framework-common/src/main/java/org/sky/framework/api/common/domain/base/BaseEntity.java
+++ sky-framework-common/src/main/java/org/sky/framework/api/common/domain/base/BaseEntity.java
@@ -1,5 +1,11 @@
 package org.sky.framework.api.common.domain.base;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
 import java.time.LocalDateTime;
 
 /**
@@ -15,11 +21,23 @@ public class BaseEntity {
     /**
      * 创建时间
      */
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime createdAt;
 
     /**
      * 更新时间
      */
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime updateAt;
 
 }
